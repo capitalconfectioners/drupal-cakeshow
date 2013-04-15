82a290f5f94f006d5317d11cc7324f575da11db4
Thomas Stephens <spiralman@gmail.com>
2013-04-14 21:32:47 -0500

    adding drupal commerce modules

--- drupal_setup.sh
+++ drupal_setup.sh
@@ -2,6 +2,8 @@
 
 INSTALL_ROOT=$1
 
+ANSWER=--yes
+
 if [ $# -lt "2" ]
 then
     read -s -p Password: PASSWORD
@@ -10,18 +12,29 @@ else
 fi
 
 echo "Installing Drupal"
-drush dl drupal-7.x --yes --destination=${INSTALL_ROOT} --drupal-project-rename=capitalconfectioners || {
+drush dl drupal-7.x ${ANSWER} --destination=${INSTALL_ROOT} --drupal-project-rename=capitalconfectioners || {
     echo "Drupal install failed"
     exit 1
 }
 
-drush vset clean_url 1 --yes
-
 cd ${INSTALL_ROOT}/capitalconfectioners
 
 echo "Installing Cakeshow site"
-drush site-install standard --yes --db-url=mysql://cakecuba_capc:${PASSWORD}@localhost/cakecuba_capitalc --site-name=CapitalConfectioners || {
+drush site-install standard ${ANSWER} --db-url=mysql://cakecuba_capc:${PASSWORD}@localhost/cakecuba_capitalc --site-name=CapitalConfectioners --account-name=admin --account-pass=${PASSWORD} || {
     echo "site install failed"
     exit 1
 }
 
+drush ${ANSWER} dl libraries,ctools,views,addressfield,rules,entity,commerce,commerce_stripe,commerce_extra_panes || exit 1
+
+drush ${ANSWER} en commerce commerce_ui || exit 1
+drush ${ANSWER} en commerce_customer commerce_customer_ui || exit 1
+drush ${ANSWER} en commerce_price || exit 1
+drush ${ANSWER} en commerce_line_item commerce_line_item_ui || exit 1
+drush ${ANSWER} en commerce_order commerce_order_ui || exit 1
+drush ${ANSWER} en commerce_checkout commerce_payment commerce_product || exit 1
+drush ${ANSWER} en commerce_cart commerce_product_pricing || exit 1
+drush ${ANSWER} en commerce_product_ui || exit 1
+drush ${ANSWER} en commerce_tax_ui || exit 1
+drush ${ANSWER} en commerce_stripe || exit 1
+drush ${ANSWER} en commerce_extra_panes || exit 1
