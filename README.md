9b0bfb14dc34694b96d85c00a1ed2795c7bb637c
Thomas Stephens <spiralman@gmail.com>
2013-04-15 21:41:39 -0500

    Making installation script idempotent, so we can re-run it

--- README.md
+++ README.md
@@ -11,18 +11,10 @@ up`. The SQL and Drupal admin passwords for vagrant instances are in
 the puppet module, but should, obviously, not be used for publically
 accessible machines.
 
-Because the `drupal_setup.sh` script is not idempotent, any changes
-you make to the Drupal installation will be wiped every time you
-`vagrant up`
-
 Drupal Installation
 ----------
 
 The `drupal_setup.sh` script can be used to install Drupal, plus any
 dependent modules. If we need to add more modules, we should add them
 to that script.
-
-**The script is not currently idempotent, so running it again will wipe
-out your existing Drupal installation, and all your data!**
-
 
--- drupal_setup.sh
+++ drupal_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 INSTALL_ROOT=$1
+INSTALL_DIR=${INSTALL_ROOT}/capitalconfectioners
 
 if [ -t 0 ]
 then
@@ -12,34 +13,77 @@ fi
 if [ $# -lt "2" ]
 then
     read -s -p Password: PASSWORD
+    echo
 else
     PASSWORD=$2
 fi
 
-echo "Installing Drupal"
-drush dl drupal-7.x ${ANSWER} --destination=${INSTALL_ROOT} --drupal-project-rename=capitalconfectioners || {
-    echo "Drupal install failed"
-    exit 1
+drupal_installed() {
+    if [ -d $INSTALL_DIR ]
+    then
+	pushd $INSTALL_DIR
+	drush status | grep -q "Drupal version"
+	INSTALLED=$?
+	popd
+	return $INSTALLED
+    else
+	return 1
+    fi
 }
 
-cd ${INSTALL_ROOT}/capitalconfectioners
+install_modules() {
+    TO_INSTALL=()
+    for module in "$@"
+    do
+	echo "(${module})"
+	if drush pm-list --status="not installed" | grep -q "(${module})"
+	then
+	    echo "${module} not installed"
+	    TO_INSTALL=( ${TO_INSTALL[@]} $module )
+	fi
+    done
 
-echo "Installing Cakeshow site"
-drush site-install standard ${ANSWER} --db-url=mysql://cakecuba_capc:${PASSWORD}@localhost/cakecuba_capitalc --site-name=CapitalConfectioners --account-name=admin --account-pass=${PASSWORD} || {
-    echo "site install failed"
-    exit 1
+    echo ${TO_INSTALL[@]}
+
+    drush ${ANSWER} en ${TO_INSTALL[@]} || exit 1
 }
+
+echo "Checking for drupal"
+
+if ! drupal_installed
+then
+    echo "Installing Drupal"
+    drush dl drupal-7.x ${ANSWER} --destination=${INSTALL_ROOT} --drupal-project-rename=capitalconfectioners || {
+	echo "Drupal install failed"
+	exit 1
+    }
+else
+    echo "Drupal already installed"
+fi
+
+cd ${INSTALL_DIR}
+
+if ! drush status | grep -q "Drupal boostrap"
+then
+    echo "Installing Cakeshow site"
+    drush site-install standard ${ANSWER} --db-url=mysql://cakecuba_capc:${PASSWORD}@localhost/cakecuba_capitalc --site-name=CapitalConfectioners --account-name=admin --account-pass=${PASSWORD} || {
+	echo "site install failed"
+	exit 1
+    }
+else
+    echo "Cakeshow site already installed"
+fi
 
 drush ${ANSWER} dl libraries,ctools,views,addressfield,rules,entity,commerce,commerce_stripe,commerce_extra_panes || exit 1
 
-drush ${ANSWER} en commerce commerce_ui || exit 1
-drush ${ANSWER} en commerce_customer commerce_customer_ui || exit 1
-drush ${ANSWER} en commerce_price || exit 1
-drush ${ANSWER} en commerce_line_item commerce_line_item_ui || exit 1
-drush ${ANSWER} en commerce_order commerce_order_ui || exit 1
-drush ${ANSWER} en commerce_checkout commerce_payment commerce_product || exit 1
-drush ${ANSWER} en commerce_cart commerce_product_pricing || exit 1
-drush ${ANSWER} en commerce_product_ui || exit 1
-drush ${ANSWER} en commerce_tax_ui || exit 1
-drush ${ANSWER} en commerce_stripe || exit 1
-drush ${ANSWER} en commerce_extra_panes || exit 1
+install_modules commerce commerce_ui
+install_modules commerce_customer commerce_customer_ui
+install_modules commerce_price
+install_modules commerce_line_item commerce_line_item_ui
+install_modules commerce_order commerce_order_ui
+install_modules commerce_checkout commerce_payment commerce_product
+install_modules commerce_cart commerce_product_pricing
+install_modules commerce_product_ui
+install_modules commerce_tax_ui
+install_modules commerce_stripe
+install_modules commerce_extra_panes
